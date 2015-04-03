3af0445ec5003e3c0ad6a4fe24770bbd23b2fb16
Felix Crux <felixc@felixcrux.com>
2015-04-03 14:30:04 -0400

    Add declarations for comment convenience getter/setters.

--- src/lib.rs
+++ src/lib.rs
@@ -98,6 +98,9 @@ extern {
     pub fn gexiv2_metadata_get_fnumber(this: *mut GExiv2Metadata) -> c_double;
     pub fn gexiv2_metadata_get_focal_length(this: *mut GExiv2Metadata) -> c_double;
     pub fn gexiv2_metadata_get_iso_speed(this: *mut GExiv2Metadata) -> c_int;
+    pub fn gexiv2_metadata_get_comment(this: *mut GExiv2Metadata) -> *const c_char;
+    pub fn gexiv2_metadata_set_comment(this: *mut GExiv2Metadata, comment: *const c_char);
+    pub fn gexiv2_metadata_clear_comment(this: *mut GExiv2Metadata);
 
     // GPS-related functions.
     pub fn gexiv2_metadata_get_gps_longitude(this: *mut GExiv2Metadata, longitude: *mut c_double) -> bool;
